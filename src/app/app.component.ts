3140b1f429e3d13a2040dbf91b7a07ca2a9593a3
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-28 22:51:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
   title = 'Localstorage_management';
+
+ tdate:string = "";
+ ttime:string = "";
+ ttask:string = "";
+
+ task:any;
+
+ tasks = new Array();
+
+
+
+
+
+ ngOnInit(): void {
+
+
+  this.tasks = JSON.parse(localStorage.getItem("task") || '[]');
+
+}
+
+
+
+
+  submit(){
+    // alert("ankita")
+
+
+    if(this.tdate == "")
+    {
+      alert("Enter date");
+      return;
+    }
+
+
+    if(this.ttime == "")
+    {
+      alert("Enter time");
+      return;
+    }
+
+
+    if(this.ttask == "")
+    {
+      alert("Enter task");
+      return;
+    }
+
+
+    let task = {date:this.tdate, time:this.ttime, task:this.ttask};
+
+
+    this.tasks = JSON.parse(localStorage.getItem("tasks") || '[]');
+
+    this.tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+
+  }
 }
